25e4e0fe1a9e23ffabca1ef2876ee6a5c881709c
AlejandroCuetoDiaz <cuetouniversidad@uma.es>
2025-05-16 20:19:27 +0200

    ImagenControllerWebTestClientIT y PacienteControllerMockMvcIT

---
+++ Práctica6/src/test/java/com/uma/example/springuma/integration/ImagenControllerWebTestClientIT.java
@@ -0,0 +1,118 @@
+package com.uma.example.springuma.integration;
+
+import java.io.File;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.MultipartBodyBuilder;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+
+import jakarta.annotation.PostConstruct;
+import com.uma.example.springuma.model.Imagen;
+import com.uma.example.springuma.model.Paciente;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ImagenControllerWebTestClientIT {
+
+    @LocalServerPort
+    private int port;
+
+    private WebTestClient client;
+
+    @PostConstruct
+    public void init() {
+        client = WebTestClient.bindToServer()
+            .baseUrl("http://localhost:" + port)
+            .build();
+    }
+
+    @Test
+    @DisplayName("Sube una imagen para un paciente correctamente")
+    void uploadImage_shouldSucceed_whenValidData() throws Exception {
+        File uploadFile = new File("./src/test/resources/uma.png");
+
+        Paciente paciente = new Paciente();
+        paciente.setId(1L);
+        paciente.setNombre("Filemon");
+        paciente.setEdad(30);
+        paciente.setDni("12345678B");
+
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+        builder.part("image", new FileSystemResource(uploadFile));
+        builder.part("paciente", paciente);
+
+        client.post()
+            .uri("/imagen")
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .body(BodyInserters.fromMultipartData(builder.build()))
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody()
+            .jsonPath("$.response").value(resp -> {
+                assertTrue(resp.toString().contains("file uploaded successfully"));
+            });
+    }
+
+    @Test
+    @DisplayName("Realiza una predicción sobre una imagen existente")
+    void predictImage_shouldReturnStatusAndScore_whenValidImageId() throws Exception {
+    
+       //Creacion paciente e imagen
+        Paciente paciente = new Paciente();
+        paciente.setId(2L);
+        paciente.setNombre("Ofelia");
+        paciente.setEdad(50);
+        paciente.setDni("12345678D");
+
+        File uploadFile = new File("./src/test/resources/uma.png");
+        MultipartBodyBuilder builder = new MultipartBodyBuilder();
+        builder.part("image", new FileSystemResource(uploadFile));
+        builder.part("paciente", paciente);
+
+        String responseBody = client.post()
+            .uri("/imagen")
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .body(BodyInserters.fromMultipartData(builder.build()))
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(String.class)
+            .returnResult()
+            .getResponseBody();
+
+        //Obtener la imagen asociada al paciente
+        List<Imagen> imagenes = client.get()
+            .uri("/imagen/paciente/2")
+            .exchange()
+            .expectStatus().isOk()
+            .returnResult(Imagen.class)
+            .getResponseBody()
+            .collectList()
+            .block();
+
+        assertNotNull(imagenes);
+        assertFalse(imagenes.isEmpty());
+
+        Long imagenId = imagenes.get(0).getId();
+
+        // Realizar la predicción
+        client.get()
+            .uri("/imagen/predict/" + imagenId)
+            .exchange()
+            .expectStatus().isOk()
+            .expectBody(String.class)
+            .consumeWith(result -> {
+                String json = result.getResponseBody();
+                assertNotNull(json);
+                assertTrue(json.contains("Cancer") || json.contains("Not cancer"));
+                assertTrue(json.contains("score"));
+            });
+    }
+    
+}
--- Práctica6/src/test/java/com/uma/example/springuma/integration/PacienteControllerMockMvcIT.java
+++ Práctica6/src/test/java/com/uma/example/springuma/integration/PacienteControllerMockMvcIT.java
@@ -1,5 +1,103 @@
 package com.uma.example.springuma.integration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-public class PacienteControllerMockMvcIT {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+
+import com.uma.example.springuma.integration.base.AbstractIntegration;
+import com.uma.example.springuma.model.Medico;
+import com.uma.example.springuma.model.Paciente;
+
+public class PacienteControllerMockMvcIT extends AbstractIntegration{
     
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("Guardar un paciente y ver que se ha guardado")
+    public void savePaciente_NotExists_SavesPaciente() throws Exception {
+
+        Medico medico = new Medico(1L, "12345678A", "Mortadelo", "Radiología");
+
+        mockMvc.perform(post("/medico")
+            .contentType("application/json")
+            .content(objectMapper.writeValueAsString(medico)))
+            .andExpect(status().isCreated());
+
+        Paciente paciente = new Paciente("Filemon", 30, "2025-05-20", "12345678B", medico);
+
+        // Guardar paciente
+         mockMvc.perform(post("/paciente")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(paciente)))
+                .andExpect(status().isCreated());
+
+        // Comprobar que se ha guardado
+         mockMvc.perform(get("/paciente/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nombre").value("Filemon"))
+                .andExpect(jsonPath("$.edad").value(30))
+                .andExpect(jsonPath("$.dni").value("12345678B"))
+                .andExpect(jsonPath("$.medico.nombre").value("Mortadelo"));
+    }
+
+    @Test
+    @DisplayName("Asociar paciente a médico, editar paciente y cambiar médico")
+    public void associateAndUpdatePaciente_ChangesMedico() throws Exception {
+
+        //Crear médicos y pacientes
+
+        Medico medico1 = new Medico(1L, "12345678A", "Mortadelo", "Radiología");
+        mockMvc.perform(post("/medico")
+        .contentType("application/json")
+        .content(objectMapper.writeValueAsString(medico1)))
+        .andExpect(status().isCreated());
+
+        Medico medico2 = new Medico(2L, "12345678C", "Bacterio", "Cardiología");
+        mockMvc.perform(post("/medico")
+        .contentType("application/json")
+        .content(objectMapper.writeValueAsString(medico2)))
+        .andExpect(status().isCreated());
+
+        Paciente paciente = new Paciente("Filemon", 30, "2025-05-20", "12345678B", medico1);
+        mockMvc.perform(post("/paciente")
+        .contentType("application/json")
+        .content(objectMapper.writeValueAsString(paciente)))
+        .andExpect(status().isCreated());
+
+        //Comprobar que el paciente está asociado al médico 1
+
+        mockMvc.perform(get("/paciente/1"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.medico.id").value(1))
+        .andExpect(jsonPath("$.medico.nombre").value("Mortadelo"));
+
+        // Cambiar datos del paciente y asignar medico2
+
+        paciente.setNombre("Filemon II");
+        paciente.setMedico(medico2);
+
+        // Actualizar paciente
+
+         mockMvc.perform(put("/paciente")
+        .contentType("application/json")
+        .content(objectMapper.writeValueAsString(paciente)))
+        .andExpect(status().isNoContent());
+
+        //Comprobar que el paciente está asociado al médico 2 y que se ha actualizado el nombre
+
+         mockMvc.perform(get("/paciente/1"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.nombre").value("Filemon II"))
+        .andExpect(jsonPath("$.medico.id").value(2))
+        .andExpect(jsonPath("$.medico.nombre").value("Bacterio"));
+        }   
+
 }
